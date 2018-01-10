a942f9d411e3bbe33c4ea9275ac03cf8a59b39ca
woodage <r-valkenburg@hotmail.com>
2018-01-10 20:09:26 +0100

    TSP calculation capital cities Netherlands.

--- travellingSalesmenProblem.py
+++ travellingSalesmenProblem.py
@@ -4,39 +4,6 @@ import urllib.request, json
 import sqlite3
 import copy
 
-conn = sqlite3.connect("cities.db")
-c = conn.cursor()
-
-cities = ['Amersfoort',
-'Baarn',
-'De Bilt',
-'Bunnik',
-'Bunschoten',
-'Eemnes',
-'Houten',
-'IJsselstein',
-'Leusden',
-'Lopik',
-'Montfoort',
-'Nieuwegein',
-'Oudewater',
-'Renswoude',
-'Rhenen',
-'De Ronde Venen',
-'Soest',
-'Stichtse Vecht',
-'Utrecht',
-'Utrechtse Heuvelrug',
-'Veenendaal',
-'Vianen',
-'Wijk bij Duurstede',
-'Woerden',
-'Woudenberg',
-'Zeist']
-
-# All the indexes of the cities.
-indexes = [i for i in range(0, len(cities))]
-
 def insert_distance_in_to_database():
     for i in range(len(cities)):
         for j in range(i + 1, len(cities)):
@@ -46,51 +13,54 @@ def insert_distance_in_to_database():
             str_url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins="+str(from_city)+"&destinations="+str(to_city)+"&key=AIzaSyAh1or4PKmF9c3A_WXvjXB7yHwCvPOKqKk"
             with urllib.request.urlopen(str_url) as url:
                 data = json.loads(url.read().decode())
+                print(str_url)
+                print(data)
                 miDistance = data['rows'][0]['elements'][0]['distance']['text']
                 kmDistance = float(miDistance.replace(" mi", "")) * 1.609344
+                from_city = cities[i].replace("+", " ")
+                to_city = cities[j].replace("+", " ")
                 # Insert a row of data
                 c.execute("INSERT INTO `distance_between_cities` VALUES ('"+from_city+"','"+to_city+"',"+ str(kmDistance) +")")
                 # Save (commit) the changes
                 conn.commit()
     conn.close()
 
-def fitness(individu):
-    totalDistance = 0.0
+""" Return the fitness result from the given individual. Lower is better. """
+def fitness(individual):
+    total_distance = 0.0
     # Calculate distance between each city in the phenotype list.
-    for i in range(len(individu.getPhenotype())):
+    for i in range(len(individual.getPhenotype())):
         if i != 0:
-            fromCity = individu.getPhenotype()[i - 1].replace(" ", "+")
-            toCity = individu.getPhenotype()[i].replace(" ", "+")
-            distance = 0.0
-            for row in c.execute("select `distance` from distance_between_cities WHERE `to`='"+toCity+"' AND `from`='"+fromCity+"'"):
-                if row is None:
-                    for row1 in c.execute("select `distance` from `distance_between_cities` WHERE  `to`='"+fromCity+"' and `from`='"+toCity+"'"):
-                        distance = row1[0]
-                else:
-                    distance = row[0]
-            totalDistance += distance
-    # Return the result of km.
-    return totalDistance
+            from_city = individual.getPhenotype()[i - 1]
+            to_city = individual.getPhenotype()[i]
+            for row in c.execute("select `distance` from distance_between_cities WHERE `to`='"+to_city+"' AND `from`='"+from_city+"'"):
+                 total_distance += copy.copy(row[0])
 
-def truncateSelection(individuals, topXIndividuals):
+            for row1 in c.execute("select `distance` from `distance_between_cities` WHERE  `to`='"+from_city+"' and `from`='"+to_city+"'"):
+                total_distance += copy.copy(row1[0])
+    # Return the result of km.
+    return total_distance
+
+""" Truncate selection will return the best individuals based on the lowest fitness. """
+def truncate_selection(individuals, top_X_Individuals):
     # Get all the fitness results.
-    fintessResults = [fitness(individual) for individual in individuals]
+    fintess_results = [fitness(individual) for individual in individuals]
     # Create a list that will contain top individuals.
     selection = []
-    for _ in range(topXIndividuals):
+    for _ in range(top_X_Individuals):
         for individual in individuals:
             # Check if the getPhenotype is the best fitnessResult.
-            if fitness(individual) == min(fintessResults):
+            if fitness(individual) == min(fintess_results):
                 # Add to selection.
                 selection.append(individual)
                 # Remove from result otherwise we get the same getPhenotype each time.
-                fintessResults.remove(min(fintessResults))
+                fintess_results.remove(min(fintess_results))
                 # Go for next best individual.
                 break
     # Return the selected individuals.
     return selection
 
-"""We use order based crossover in this example. """
+""" We use order based crossover in this example. """
 def crossover(population):
     # Define a list with children to return.
     children = []
@@ -99,7 +69,7 @@ def crossover(population):
         # Make with everyone a child...
         for j in range(i + 1, len(population)):
             # Get child.
-            child = Individu()
+            child = Individual()
             # Assign mother genes to the child.
             child.genes = copy.copy(population[j].genes)
             # Get halve a gen from the father.
@@ -118,20 +88,21 @@ def crossover(population):
 """ We use insertion mutate in this example. """
 def mutate(population):
     # Mutate each individu in the population.
-    for individu in population:
+    for individual in population:
         # Get random indexes from the genes.
-        random_index_0 = random.randint(0, len(individu.genes) - 1)
-        random_index_1 = random.randint(0, len(individu.genes) - 1)
+        random_index_0 = random.randint(0, len(individual.genes) - 1)
+        random_index_1 = random.randint(0, len(individual.genes) - 1)
         # Indexes will not be the same.
         while random_index_0 == random_index_1:
-            random_index_1 = random.randint(0, len(individu.genes) - 1)
+            random_index_1 = random.randint(0, len(individual.genes) - 1)
         # Create a temp value.
-        tmp = individu.genes[random_index_0]
-        individu.genes[random_index_0] = individu.genes[random_index_1]
-        individu.genes[random_index_1] = tmp
+        tmp = individual.genes[random_index_0]
+        individual.genes[random_index_0] = individual.genes[random_index_1]
+        individual.genes[random_index_1] = tmp
     return population
 
-class Individu(object):
+""" Class that will represent the indivudual. """
+class Individual(object):
 
     def __init__(self):
         copyIndexes = copy.copy(indexes)
@@ -141,18 +112,43 @@ class Individu(object):
     def getPhenotype(self):
         return [cities[i] for i in self.genes]
 
-indi = Individu()
+""" Return the best individual from the last generation. """
+def getBestIndividualFromEA(amount_individuals, amount_generations):
+    # Generate a random population.
+    population = [Individual() for _ in range(0, amount_individuals)]
+    # Loop each amount generation.
+    for current_generation in range(amount_generations):
+        print("CALCULATING GENERATION " + str(current_generation))
+        # Get the best population based on their fitness.
+        best_population = truncate_selection(population, int(len(population) / 2))
+        # Perform a crossover with the best population. This will return kids that can be seen as new individuals.
+        kids = crossover(best_population)
+        # Create the new population.
+        population = best_population + mutate(kids)[:int(len(population) / 2)]
+        print( min([fitness(individual) for individual in population]))
+    # Calculate the best fitness.
+    bestFitness = min([fitness(individual) for individual in population])
+    # Loop each individual from last generation.
+    for individual in population:
+        # Check if the fitness of the individual matches with the bestFitness.
+        if fitness(individual) == bestFitness:
+            # Return individual.
+            return individual
+
+# Make connection.
+conn = sqlite3.connect("cities.db")
+c = conn.cursor()
+# Cities.
+cities = ['Groningen, NL','Leeuwarden, NL','Assen, NL','Zwolle, NL','Lelystad, NL','Arnhem, NL','Utrecht, NL','Haarlem, NL','Den Haag, NL','Middelburg, NL','Den Bosch, NL','Maastricht, NL']
+# All the indexes of the cities.
+indexes = [i for i in range(0, len(cities))]
+# Constants for the algorithm.
 AMOUNT_INDIVIDUALS = 30
 AMOUNT_GENERATIONS = 100
-
-population = [Individu() for _ in range(0, AMOUNT_INDIVIDUALS)]
-
-for current_generation in range(AMOUNT_GENERATIONS):
-    print("CALCULATING GENERATION " + str(current_generation))
-    best_population = truncateSelection(population, int(len(population) / 2))
-    kids = crossover(best_population)
-    population = best_population + mutate(kids)[:int(len(population) / 2)]
-    print(min([fitness(individual) for individual in population]))
-    print(len(population))
-
+# Get best individual from algorithm.
+best_individual = getBestIndividualFromEA(AMOUNT_INDIVIDUALS, AMOUNT_GENERATIONS)
+# Print status.
+print(best_individual.getPhenotype())
+print(fitness(best_individual))
+# Close database connection.
 c.close()
